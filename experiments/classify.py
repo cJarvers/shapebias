8ee795570fde70cc84d7b255b10b38d762ecd084
Christian Jarvers <christian.jarvers@gmx.de>
2022-11-24 18:28:19 +0100

    Added command line arguments to classification script

--- experiments/classify.py
+++ experiments/classify.py
@@ -2,56 +2,68 @@
 # PascalVOC originally, they have twenty classes. Most networks are trained
 # on ImageNet with 1000 classes, so the predictions have to be mapped
 # onto the 20 PascalVOC categories (and one "background" class).
+import argparse
 import numpy as np
+from matplotlib import pyplot as plt
 import torch
 torch.set_grad_enabled(False)
 from torch.utils.data import DataLoader
-from torchvision.models import resnet50, ResNet50_Weights
-from torchvision.transforms import ToTensor, Resize, Compose
 import sys
 sys.path.insert(0, "../src")
-from datasets import SilhouetteDataset
-from mappings import get_image_bbox, get_image_fg_bbox, get_image_bg_bbox, get_silhouette_bbox
+from datasets import loaddataset
+from loadnetworks import loadnetwork
 from helpers.imagenet_synsets import imagenet2voc
 
+# Parse command line
+parser = argparse.ArgumentParser(description="Test for classification performance on modified datasets.")
+parser.add_argument("--datasets", type=str, nargs="+", required=True, help="Datasets / image types to use as inputs.")
+parser.add_argument("--network", type=str, default="resnet50", help="Network to extract activations from.")
+parser.add_argument("--set", type=str, default="val", help="PascalVOC image set to use (e.g., 'val').")
+parser.add_argument("-b", "--batchsize", type=int, default=32)
+args = parser.parse_args()
+
 # load data
-images = SilhouetteDataset("../data", image_set="trainval",
-    filters=["single", "occluded", "truncated"],
-    mapping=get_image_bbox
-)
-foregrounds = SilhouetteDataset("../data", image_set="trainval",
-    filters=["single", "occluded", "truncated"],
-    mapping=get_image_fg_bbox,
-    sinds=images.sinds, dinds=images.dinds
-)
-backgrounds = SilhouetteDataset("../data", image_set="trainval",
-    filters=["single", "occluded", "truncated"],
-    mapping=get_image_bg_bbox,
-    sinds=images.sinds, dinds=images.dinds
-)
-silhouettes = SilhouetteDataset("../data", image_set="trainval",
-    filters=["single", "occluded", "truncated"],
-    mapping=get_silhouette_bbox,
-    sinds=images.sinds, dinds=images.dinds
-)
-imloader = DataLoader(images, batch_size=32)
-fgloader = DataLoader(foregrounds, batch_size=32)
-bgloader = DataLoader(backgrounds, batch_size=32)
-shloader = DataLoader(silhouettes, batch_size=32)
+datasets = {}
+for imgtype in args.datasets:
+    datasets[imgtype], sinds, dinds = loaddataset(imgtype, image_set=args.set)
+loaders = {imgtype: DataLoader(dset, batch_size=args.batchsize) for imgtype, dset in datasets.items()}
 
 # set up network
-weights = ResNet50_Weights.IMAGENET1K_V2
-net = resnet50(weights=weights)
+net = loadnetwork(args.network, layers=None)
 
-total = 0
-correct = {"image     ": 0, "foreground": 0, "background": 0, "silhouette": 0}
-for (img, lbl), (fg, _), (bg, _), (sil, _) in zip(imloader, fgloader, bgloader, shloader):
-    for k, x in {"image     ": img, "foreground": fg, "background": bg, "silhouette": sil}.items():
-        out = net(x)
+# get network predictions on dataset(s)
+predictions = {}
+for dset, loader in loaders.items():
+    predictions[dset] = {
+        "prediction": [],
+        "label": []
+    }
+    for img, lbl in loader:
+        out = net(img)
         pred = imagenet2voc[out.argmax(dim=1).numpy()]
-        correct[k] += np.sum(pred == lbl.numpy())
-    total += lbl.size(0)
-print("Total number of images: ", total)
-print("Correct responses / accuracies:")
-for k, c in correct.items():
-    print(k, "\t", c, "correct", f"\t({c / total * 100:1.3f}%)")
+        predictions[dset]["prediction"].append(pred)
+        predictions[dset]["label"].append(lbl.numpy())
+# Postprocessing: concatenate tensors / arrays from all batches
+for dset in predictions:
+    predictions[dset]["prediction"] = np.concatenate(predictions[dset]["prediction"])
+    predictions[dset]["label"] = np.concatenate(predictions[dset]["label"])
+# Check that all labels are the same across all datasets (i.e., that the same images were used)
+for dset1 in predictions:
+    for dset2 in predictions:
+        if not (predictions[dset1]["label"] == predictions[dset2]["label"]).all():
+            raise ValueError(f"Labels not identical! Datasets {dset1} and {dset2} contain different images!")
+
+# Calculate accuracies
+accuracies = {}
+for dset in predictions:
+    total = predictions[dset]["prediction"].size
+    correct = (predictions[dset]["prediction"] == predictions[dset]["label"]).sum()
+    accuracies[dset] = {"acc": correct / total}
+
+# Print and plot results
+for dset in predictions:
+    print(dset.ljust(30), f"{accuracies[dset]['acc']*100:1.3f}%")
+
+plt.bar(range(len(args.datasets)), height=[accuracies[dset]["acc"] for dset in accuracies])
+#plt.boxplot([accuracies[dset]["perm_accs"] for dset in accuracies])
+plt.show()
