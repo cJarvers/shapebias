4da92ac0af28e26d31ef5a6d62dd36bfb44e4d94
Christian Jarvers <christian.jarvers@gmx.de>
2023-03-16 12:10:36 +0100

    Fixed error in fdr adjustment when no p values satisfy criterion

--- src/fdr.py
+++ src/fdr.py
@@ -6,5 +6,8 @@ def fdrcorrection(pvals, Q=0.05):
     sorted = np.sort(pvals)
     ranks = np.arange(1, len(sorted)+1)
     crit = sorted < ranks / m * Q
+    if not crit.any():
+        # if no p-values fulfill the criterion, return only zeros (none are significant)
+        return np.zeros(m, dtype=bool)
     max_p = np.max(sorted[crit])
     return pvals <= max_p
